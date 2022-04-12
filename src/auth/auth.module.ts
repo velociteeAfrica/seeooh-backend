857fe6928ab1176efac3335b5b3e03980345b67b
Tunde Alabi <tundealabi4780@gmail.com>
2022-04-12 19:00:26 +0100

    fix 5 for nodemailer heroku error

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,6 +10,7 @@ import { PublisherModule } from '../publisher/publisher.module';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigService } from '@nestjs/config';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { AppLoggerService } from '../logger/logger.service';
 
 @Module({
   imports: [
@@ -18,28 +19,50 @@ import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handleba
       'publishers',
     ),
     MailerModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => ({
-        transport: {
-          host: configService.get('MAIL_OUTGOING_SERVER'),
-          port: 465,
-          secure: false,
-          auth: {
-            user: configService.get('MAIL_INCOMING_USERNAME'),
-            pass: configService.get('MAIL_INCOMING_PASSWORD'),
+      useFactory: async (
+        configService: ConfigService,
+        loggerService: AppLoggerService,
+      ) => {
+        loggerService.log(
+          'MAIL_OUTGOING_SERVER',
+          configService.get('MAIL_OUTGOING_SERVER'),
+        );
+        loggerService.log(
+          'MAIL_OUTGOING_PORT',
+          configService.get('MAIL_OUTGOING_PORT'),
+        );
+        loggerService.log(
+          'MAIL_INCOMING_USERNAME',
+          configService.get('MAIL_INCOMING_USERNAME'),
+        );
+        loggerService.log(
+          'MAIL_INCOMING_PASSWORD',
+          configService.get('MAIL_INCOMING_PASSWORD'),
+        );
+        loggerService.log('process', process.cwd());
+        return {
+          transport: {
+            host: configService.get('MAIL_OUTGOING_SERVER'),
+            port: 465,
+            secure: false,
+            auth: {
+              user: configService.get('MAIL_INCOMING_USERNAME'),
+              pass: configService.get('MAIL_INCOMING_PASSWORD'),
+            },
           },
-        },
-        defaults: {
-          from: 'Seeooh <support@seeooh.app>',
-        },
-        template: {
-          dir: process.cwd() + '/src/templates/',
-          adapter: new HandlebarsAdapter(),
-          options: {
-            strict: true,
+          defaults: {
+            from: 'Seeooh <support@seeooh.app>',
           },
-        },
-      }),
-      inject: [ConfigService],
+          template: {
+            dir: process.cwd() + '/src/templates/',
+            adapter: new HandlebarsAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        };
+      },
+      inject: [ConfigService, AppLoggerService],
     }),
     JwtModule.register({}),
     PublisherModule,
